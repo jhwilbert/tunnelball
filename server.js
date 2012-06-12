945b892e854f9b61456b49c4f8e8088ed73c45af
Joao Wilbert <joaowilbert@joaowilbert.local>
2012-06-12 10:39:03 +0100

    added handler

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var http = require('http');
-http.createServer(function (req, res) {
+app = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\nApp (dropball) is running on Node.JS ' + process.version);
 }).listen(process.env['app_port'] || 3000);
+
+app.get('/', function (req, res) {
+	res.sendfile(__dirname + '/index.html');
+});
