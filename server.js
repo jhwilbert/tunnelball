452a0ee44437b0fcd5b01e2e763b5344b370a671
Joao Wilbert <joaowilbert@joaowilbert.local>
2012-06-12 10:59:45 +0100

    included static folders

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ var io = require('socket.io').listen(app);
 
 app.listen(process.env['app_port'] || 3000);
 
+app.use("/js", express.static(__dirname + '/js'));
+app.use("/css", express.static(__dirname + '/css'));
+app.use("/imgs", express.static(__dirna,e + '/imgs'));
+
+
 app.get('/', function (req, res) {
 	res.sendfile(__dirname + '/index.html');
 });
