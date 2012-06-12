fbbc4699374f5811777ef9064b9cbb19bbd00912
Joao Wilbert <joaowilbert@joaowilbert.local>
2012-06-12 10:40:30 +0100

    commented faulity bit

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ app = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\nApp (dropball) is running on Node.JS ' + process.version);
 }).listen(process.env['app_port'] || 3000);
-
+/*
 app.get('/', function (req, res) {
 	res.sendfile(__dirname + '/index.html');
 });
+*/
