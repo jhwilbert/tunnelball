7571f67bc0b9a13dd5bfc220f21dadfc60209532
Joao Wilbert <jhwilbert@gmail.com>
2012-06-15 03:25:58 -0700

    added test function

--- server.js
+++ server.js
@@ -7,6 +7,7 @@
 /******************************************************************************/
 var express = require('express');
 var app = express.createServer()
+
 var io = require('socket.io').listen(app);
 
 /*app.listen(process.env['app_port'] || 3000);*/
@@ -20,6 +21,8 @@ app.get('/', function (req, res) {
 	res.sendfile(__dirname + '/index.html');
 });
 
+function test() {
+}
 /******************************************************************************/
 /*  Client Object									                          */
 /******************************************************************************/
